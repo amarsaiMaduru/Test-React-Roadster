95953ba164160c7b229a231c6348b52a4e6ca602
amarsaiMaduru <167751067+amarsaiMaduru@users.noreply.github.com>
2024-07-17 12:24:07 +0530

    Commit App.js
    Important Component to run the file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import RegisterForm from './rough/roughForm';
+// import Routeing from './router';
+// import RoughForm from './rough/roughForm';
+// import CounterObject from './stateobject';
+// import ObjectArray from './statewitharray';
 
+// import UseContaxt from './hooks/useeffect';
+// import ReducerCounter from './hooks/usereducer/index1';
+// import TodoPractice from './hooks/todo';
+// import UseMemoo from './hooks/usememo';
+// import Comparision from './useeffectvsusememo';
+// import ComponentB from './hooks/usecontext/components';
+
+
+// import ComponentA from './hooks/usecontext';
+// import ComponentC from './hooks/usecontext/components/indexc';
+// import PracticeAxios from './axios';
+// import CreateForm from './form';
+// import Higher from './hoc';
+// import Hover from './hoc/hover';
+
+//  export const UserContext=createContext()
 function App() {
+  // const object={
+  //   name:"amar"
+  // }
+  // console.log("context",object)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     {/* <CreateForm/> */}
+     {/* <PracticeAxios/> */}
+     {/* <Higher/>
+     <Hover/> */}
+{/* <UserContext.Provider value={object}>
+  <ComponentC/>
+ {/* <ComponentB/> */}
+  {/* </UserContext.Provider> */} 
+  {/* <CounterObject/>
+  <ObjectArray/>
+  <UseContaxt/>
+  <ReducerCounter/>
+  <TodoPractice/>
+  <UseMemoo/>
+  <Comparision/> */}
+  {/* <RoughForm/> */}
+  {/* <Routeing/> */}
+  <RegisterForm/>
     </div>
   );
 }
